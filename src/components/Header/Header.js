bb22fb4126dee9c7fc3a16ca5320647683d9d6f2
toritheterrible <torimpham@gmail.com>
2020-11-17 19:24:45 -0800

    added links to social

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,10 @@ export const Header = (props) => {
                 <p className="headerText">I am a California based Web Developer</p>
                 <p className="headerText">Full Stack MERN - Mongo, Express, React, and Node</p>
                 <div class="linkList">
-                    <span id="links"><i class="fab fa-facebook-f fa-lg"></i></span>
-                    <span id="links"><i class="fab fa-linkedin-in fa-lg"></i></span>
-                    <span id="links"><i class="fab fa-instagram fa-lg"></i></span>
-                    <span id="links"><i class="fab fa-github fa-lg"></i></span>
+                    <span id="links"><a target="_blank" rel="noreferrer" href="https://www.facebook.com/tori.pham.54/"><i class="fab fa-facebook-f fa-lg"></i></a></span>
+                    <span id="links"><a target="_blank" rel="noreferrer" href="www.linkedin.com/in/toribromma"><i class="fab fa-linkedin-in fa-lg"></i></a></span>
+                    <span id="links"><a target="_blank" rel="noreferrer" href="https://www.instagram.com/makinghist0ri/"><i class="fab fa-instagram fa-lg"></i></a></span>
+                    <span id="links"><a target="_blank" rel="noreferrer" href="https://github.com/toribromma"><i class="fab fa-github fa-lg"></i></a></span>
                 </div>
             </header>
         </section>
